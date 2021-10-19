cf014b3183d4038a95554fa7bdd233d9c3f24548
gzagatti <gzagatti@users.noreply.github.com>
2021-10-19 09:26:06 +0800

    migrates to Graph.jl

--- src/lines.jl
+++ src/lines.jl
@@ -1,19 +1,27 @@
 """
 Return lines and arrow heads
 """
-function graphline(g, locs_x, locs_y, nodesize::Vector{T}, arrowlength, angleoffset) where {T<:Real}
-    lines = Array{Vector{Tuple{Float64,Float64}}}(undef, ne(g))
-    arrows = Array{Vector{Tuple{Float64,Float64}}}(undef, ne(g))
-    for (e_idx, e) in enumerate(edges(g))
+function graphline(g::AbstractGraph{T}, locs_x, locs_y, nodemapping, nodesize::Union{R, Vector{R}}, arrowlength, angleoffset) where {T<:Integer, R<:Real}
+    NE = isnothing(nodemapping) ? ne(g) : reduce(+, map(e -> ((src(e) in nodemapping) && (dst(e) in nodemapping)) ? 1 : 0, edges(g)), init=0)
+    iter = isnothing(nodemapping) ? edges(g) : [e for e in edges(g) if ((src(e) in nodemapping) && (dst(e) in nodemapping))]
+    lines = Array{Vector{Tuple{Float64,Float64}}}(undef, NE)
+    arrows = Array{Vector{Tuple{Float64,Float64}}}(undef, NE)
+    for (e_idx, e) in enumerate(iter)
         i = src(e)
         j = dst(e)
+        if !isnothing(nodemapping)
+            i = findfirst(nodemapping .== i)
+            j = findfirst(nodemapping .== j)
+        end
         Δx = locs_x[j] - locs_x[i]
         Δy = locs_y[j] - locs_y[i]
         θ = atan(Δy,Δx)
-        startx = locs_x[i] + nodesize[i]*cos(θ)
-        starty = locs_y[i] + nodesize[i]*sin(θ)
-        endx = locs_x[j] + nodesize[j]*cos(θ+π)
-        endy = locs_y[j] + nodesize[j]*sin(θ+π)
+        adj = θ > 0 ? (θ > π/2 ? abs(θ - π) : θ) : (θ < -π/2 ? θ + π : abs(θ))
+        adj = (1 + (0.8*adj)/π)
+        startx = locs_x[i] + adj*getcycle(nodesize, i)*cos(θ)
+        starty = locs_y[i] + adj*getcycle(nodesize, i)*sin(θ)
+        endx = locs_x[j] + adj*getcycle(nodesize, j)*cos(θ+π)
+        endy = locs_y[j] + adj*getcycle(nodesize, j)*sin(θ+π)
         lines[e_idx] = [(startx, starty), (endx, endy)]
         arr1, arr2 = arrowcoords(θ, endx, endy, arrowlength, angleoffset)
         arrows[e_idx] = [arr1, (endx, endy), arr2]
@@ -21,78 +29,86 @@ function graphline(g, locs_x, locs_y, nodesize::Vector{T}, arrowlength, angleoff
     lines, arrows
 end
 
-function graphline(g::AbstractGraph{T}, locs_x, locs_y, nodesize::Real, arrowlength, angleoffset) where {T<:Integer}
-    lines = Array{Vector{Tuple{Float64,Float64}}}(undef, ne(g))
-    arrows = Array{Vector{Tuple{Float64,Float64}}}(undef, ne(g))
-    for (e_idx, e) in enumerate(edges(g))
+function graphline(g::AbstractGraph{T}, locs_x, locs_y, nodemapping, nodesize::Union{R, Vector{R}}) where {T<:Integer, R<:Real}
+    NE = isnothing(nodemapping) ? ne(g) : reduce(+, map(e -> ((src(e) in nodemapping) && (dst(e) in nodemapping)) ? 1 : 0, edges(g)), init=0)
+    iter = isnothing(nodemapping) ? edges(g) : [e for e in edges(g) if ((src(e) in nodemapping) && (dst(e) in nodemapping))]
+    lines = Array{Vector{Tuple{Float64,Float64}}}(undef, NE)
+    for (e_idx, e) in enumerate(iter)
         i = src(e)
         j = dst(e)
+        if !isnothing(nodemapping)
+            i = findfirst(nodemapping .== i)
+            j = findfirst(nodemapping .== j)
+        end
         Δx = locs_x[j] - locs_x[i]
         Δy = locs_y[j] - locs_y[i]
         θ = atan(Δy,Δx)
-        startx = locs_x[i] + nodesize*cos(θ)
-        starty = locs_y[i] + nodesize*sin(θ)
-        endx = locs_x[j] + nodesize*cos(θ+π)
-        endy = locs_y[j] + nodesize*sin(θ+π)
-        lines[e_idx] = [(startx, starty), (endx, endy)]
-        arr1, arr2 = arrowcoords(θ, endx, endy, arrowlength, angleoffset)
-        arrows[e_idx] = [arr1, (endx, endy), arr2]
-    end
-    lines, arrows
-end
-
-function graphline(g::AbstractGraph{T}, locs_x, locs_y, nodesize::Vector{<:Real}) where {T<:Integer}
-    lines = Array{Vector{Tuple{Float64,Float64}}}(undef, ne(g))
-    for (e_idx, e) in enumerate(edges(g))
-        i = src(e)
-        j = dst(e)
-        Δx = locs_x[j] - locs_x[i]
-        Δy = locs_y[j] - locs_y[i]
-        θ = atan(Δy,Δx)
-        startx = locs_x[i] + nodesize[i]*cos(θ)
-        starty = locs_y[i] + nodesize[i]*sin(θ)
-        endx = locs_x[j] + nodesize[j]*cos(θ+π)
-        endy = locs_y[j] + nodesize[j]*sin(θ+π)
+        adj = θ > 0 ? (θ > π/2 ? abs(θ - π) : θ) : (θ < -π/2 ? θ + π : abs(θ))
+        adj = (1 + (0.8*adj)/π)
+        startx = locs_x[i] + adj*getcycle(nodesize, i)*cos(θ)
+        starty = locs_y[i] + adj*getcycle(nodesize, i)*sin(θ)
+        endx = locs_x[j] + adj*getcycle(nodesize, j)*cos(θ+π)
+        endy = locs_y[j] + adj*getcycle(nodesize, j)*sin(θ+π)
         lines[e_idx] = [(startx, starty), (endx, endy)]
     end
     lines
 end
 
-function graphline(g::AbstractGraph{T}, locs_x, locs_y, nodesize::Real) where {T<:Integer}
-    lines = Array{Vector{Tuple{Float64,Float64}}}(undef, ne(g))
-    for (e_idx, e) in enumerate(edges(g))
-        i = src(e)
-        j = dst(e)
-        Δx = locs_x[j] - locs_x[i]
-        Δy = locs_y[j] - locs_y[i]
-        θ = atan(Δy,Δx)
-        startx = locs_x[i] + nodesize*cos(θ)
-        starty = locs_y[i] + nodesize*sin(θ)
-        endx = locs_x[j] + nodesize*cos(θ+π)
-        endy = locs_y[j] + nodesize*sin(θ+π)
-        lines[e_idx] = [(startx, starty), (endx, endy)]
+function stubline(g::AbstractGraph{T}, locs_x, locs_y, nodemapping, nodesize::Union{R, Vector{R}}) where {T<:Integer, R<:Real}
+    lines = Vector{Tuple{Float64, Float64}}[]
+    for (i, node) in enumerate(nodemapping)
+        visible = [i for (i, n) in enumerate(nodemapping) if n in neighbors(g, node)]
+        Δx = [locs_x[j] - locs_x[i] for j in visible]
+        Δy = [locs_y[j] - locs_y[i] for j in visible]
+        visible_θ = [atan(Δy[i], Δx[i]) for i in 1:length(Δx)]
+        stubs = length(neighbors(g, node)) - length(visible)
+        θ = 2*π / stubs
+        for k in 0:(stubs-1)
+            angle = θ*k
+            if angle > π
+                angle = angle - 2π
+            end
+            if angle in visible_θ
+                angle += π / (2*stubs) 
+            end
+            adj = angle > 0 ? (angle > π/2 ? abs(angle - π) : angle) : (angle < -π/2 ? angle + π : abs(angle))
+            adj = (0.8*adj)/π
+            startx = locs_x[i] + (1+adj)*getcycle(nodesize, i)*cos(angle)
+            starty = locs_y[i] + (1+adj)*getcycle(nodesize, i)*sin(angle)
+            endx = locs_x[i] + (2.5+adj)*getcycle(nodesize, i)*cos(angle)
+            endy = locs_y[i] + (2.5+adj)*getcycle(nodesize, i)*sin(angle)
+            push!(lines, [(startx, starty), (endx, endy)])
+        end
     end
-    return lines
+    lines
 end
 
-function graphcurve(g::AbstractGraph{T}, locs_x, locs_y, nodesize::Vector{<:Real}, arrowlength, angleoffset, outangle=pi/5) where {T<:Integer}
+function graphcurve(g::AbstractGraph{T}, locs_x, locs_y, nodemapping, nodesize::Union{R, Vector{R}}, arrowlength, angleoffset, outangle=pi/5) where {T<:Integer, R<:Real}
+    NE = isnothing(nodemapping) ? ne(g) : reduce(+, map(e -> ((src(e) in nodemapping) && (dst(e) in nodemapping)) ? 1 : 0, edges(g)), init=0)
+    iter = isnothing(nodemapping) ? edges(g) : [e for e in edges(g) if ((src(e) in nodemapping) && (dst(e) in nodemapping))]
     curves = Matrix{Tuple{Float64,Float64}}(undef, ne(g), 4)
-    arrows = Array{Vector{Tuple{Float64,Float64}}}(undef, ne(g))
-    for (e_idx, e) in enumerate(edges(g))
+    arrows = Array{Vector{Tuple{Float64,Float64}}}(undef, NE)
+    for (e_idx, e) in enumerate(iter)
         i = src(e)
         j = dst(e)
+        if !isnothing(nodemapping)
+            i = findfirst(nodemapping .== i)
+            j = findfirst(nodemapping .== j)
+        end
         Δx = locs_x[j] - locs_x[i]
         Δy = locs_y[j] - locs_y[i]
         θ = atan(Δy,Δx)
-        startx = locs_x[i] + nodesize[i]*cos(θ+outangle)
-        starty = locs_y[i] + nodesize[i]*sin(θ+outangle)
-        endx = locs_x[j] + nodesize[j]*cos(θ+π-outangle)
-        endy = locs_y[j] + nodesize[j]*sin(θ+π-outangle)
+        adj = θ > 0 ? (θ > π/2 ? abs(θ - π) : θ) : (θ < -π/2 ? θ + π : abs(θ))
+        adj = (1 + (0.8*adj)/π)
+        startx = locs_x[i] + adj*getcycle(nodesize, i)*cos(θ+outangle)
+        starty = locs_y[i] + adj*getcycle(nodesize, i)*sin(θ+outangle)
+        endx = locs_x[j] + adj*getcycle(nodesize, j)*cos(θ+π-outangle)
+        endy = locs_y[j] + adj*getcycle(nodesize, j)*sin(θ+π-outangle)
 
         d = hypot(endx-startx, endy-starty)
 
         if i == j
-            d = 2 * π * nodesize[i]
+            d = 2 * π * getcycle(nodesize, i)
         end
 
         curves[e_idx, :] = curveedge(startx, starty, endx, endy, θ, outangle, d)
@@ -103,70 +119,26 @@ function graphcurve(g::AbstractGraph{T}, locs_x, locs_y, nodesize::Vector{<:Real
     return curves, arrows
 end
 
-function graphcurve(g, locs_x, locs_y, nodesize::Real, arrowlength, angleoffset, outangle=pi/5)
-    curves = Matrix{Tuple{Float64,Float64}}(undef, ne(g), 4)
-    arrows = Array{Vector{Tuple{Float64,Float64}}}(undef, ne(g))
-    for (e_idx, e) in enumerate(edges(g))
+function graphcurve(g::AbstractGraph{T}, locs_x, locs_y, nodemapping, nodesize::Union{R, Vector{R}}, outangle) where {T<:Integer, R<:Real}
+    NE = isnothing(nodemapping) ? ne(g) : reduce(+, map(e -> ((src(e) in nodemapping) && (dst(e) in nodemapping)) ? 1 : 0, edges(g)), init=0)
+    iter = isnothing(nodemapping) ? edges(g) : [e for e in edges(g) if ((src(e) in nodemapping) && (dst(e) in nodemapping))]
+    curves = Matrix{Tuple{Float64,Float64}}(undef, NE, 4)
+    for (e_idx, e) in enumerate(iter)
         i = src(e)
         j = dst(e)
-        Δx = locs_x[j] - locs_x[i]
-        Δy = locs_y[j] - locs_y[i]
-        θ = atan(Δy,Δx)
-        startx = locs_x[i] + nodesize*cos(θ+outangle)
-        starty = locs_y[i] + nodesize*sin(θ+outangle)
-        endx = locs_x[j] + nodesize*cos(θ+π-outangle)
-        endy = locs_y[j] + nodesize*sin(θ+π-outangle)
-
-        d = hypot(endx-startx, endy-starty)
-
-        if i == j
-            d = 2 * π * nodesize
+        if !isnothing(nodemapping)
+            i = findfirst(nodemapping .== i)
+            j = findfirst(nodemapping .== j)
         end
-
-        curves[e_idx, :] = curveedge(startx, starty, endx, endy, θ, outangle, d)
-
-        arr1, arr2 = arrowcoords(θ-outangle, endx, endy, arrowlength, angleoffset)
-        arrows[e_idx] = [arr1, (endx, endy), arr2]
-    end
-    return curves, arrows
-end
-
-function graphcurve(g, locs_x, locs_y, nodesize::Real, outangle)
-    curves = Matrix{Tuple{Float64,Float64}}(undef, ne(g), 4)
-    for (e_idx, e) in enumerate(edges(g))
-        i = src(e)
-        j = dst(e)
         Δx = locs_x[j] - locs_x[i]
         Δy = locs_y[j] - locs_y[i]
         θ = atan(Δy,Δx)
-        startx = locs_x[i] + nodesize*cos(θ+outangle)
-        starty = locs_y[i] + nodesize*sin(θ+outangle)
-        endx = locs_x[j] + nodesize*cos(θ+π-outangle)
-        endy = locs_y[j] + nodesize*sin(θ+π-outangle)
-
-        d = hypot(endx-startx, endy-starty)
-
-        if i == j
-            d = 2 * π * nodesize
-        end
-
-        curves[e_idx, :] = curveedge(startx, starty, endx, endy, θ, outangle, d)
-    end
-    return curves
-end
-
-function graphcurve(g::AbstractGraph{T}, locs_x, locs_y, nodesize::Vector{<:Real}, outangle) where {T<:Integer}
-    curves = Matrix{Tuple{Float64,Float64}}(undef, ne(g), 4)
-    for (e_idx, e) in enumerate(edges(g))
-        i = src(e)
-        j = dst(e)
-        Δx = locs_x[j] - locs_x[i]
-        Δy = locs_y[j] - locs_y[i]
-        θ = atan(Δy,Δx)
-        startx = locs_x[i] + nodesize[i]*cos(θ+outangle)
-        starty = locs_y[i] + nodesize[i]*sin(θ+outangle)
-        endx = locs_x[j] + nodesize[j]*cos(θ+π-outangle)
-        endy = locs_y[j] + nodesize[j]*sin(θ+π-outangle)
+        adj = θ > 0 ? (θ > π/2 ? abs(θ - π) : θ) : (θ < -π/2 ? θ + π : abs(θ))
+        adj = (1 + (0.8*adj)/π)
+        startx = locs_x[i] + adj*getcycle(nodesize, i)*cos(θ+outangle)
+        starty = locs_y[i] + adj*getcycle(nodesize, i)*sin(θ+outangle)
+        endx = locs_x[j] + adj*getcycle(nodesize, j)*cos(θ+π-outangle)
+        endy = locs_y[j] + adj*getcycle(nodesize, j)*sin(θ+π-outangle)
 
         d = hypot(endx-startx, endy-starty)
 
--- src/plot.jl
+++ src/plot.jl
@@ -1,5 +1,13 @@
 using Colors
 
+function getcycle(x, i)
+    if typeof(x) <: AbstractArray
+        return x[((i-1) % length(x))+1]
+    else
+        return x
+    end
+end
+
 # this function is copy from [GraphLayout.jl](https://github.com/IainNZ/GraphLayout.jl) and make some modifications.
 """
 Given a graph and two vectors of X and Y coordinates, returns
@@ -19,6 +27,9 @@ Default: `spring_layout`
 `locs_x, locs_y`
 Locations of the nodes. Can be any units you want,
 but will be normalized and centered anyway
+
+`nodemapping`
+Optional. A vector representing the mapped graph vertices. Default: `nothing`
 
 `NODESIZE`
 Optional. Max size for the nodes. Default: `3.0/sqrt(N)`
@@ -87,6 +98,7 @@ Optional. Angular width in radians for the arrows. Default: `π/9 (20 degrees)`
 """
 function gplot(g::AbstractGraph{T},
     locs_x_in::Vector{R}, locs_y_in::Vector{R};
+    nodemapping = nothing,
     nodelabel = nothing,
     nodelabelc = colorant"black",
     nodelabelsize = 1.0,
@@ -103,7 +115,7 @@ function gplot(g::AbstractGraph{T},
     edgelabeldistx = 0.0,
     edgelabeldisty = 0.0,
     nodesize = 1.0,
-    NODESIZE = 0.25 / sqrt(nv(g)),
+    NODESIZE = 0.1 / sqrt(nv(g)),
     nodefillc = colorant"turquoise",
     nodestrokec = nothing,
     nodestrokelw = 0.0,
@@ -113,8 +125,8 @@ function gplot(g::AbstractGraph{T},
     outangle = pi/5) where {T <:Integer, R <: Real}
 
     length(locs_x_in) != length(locs_y_in) && error("Vectors must be same length")
-    N = nv(g)
-    NE = ne(g)
+    N = isnothing(nodemapping) ? nv(g) : length(nodemapping)
+    NE = isnothing(nodemapping) ? ne(g) : reduce(+, map(e -> ((src(e) in nodemapping) && (dst(e) in nodemapping)) ? 1 : 0, edges(g)), init=0)
     if nodelabel != nothing && length(nodelabel) != N
         error("Must have one label per node (or none)")
     end
@@ -125,23 +137,7 @@ function gplot(g::AbstractGraph{T},
     locs_x = Float64.(locs_x_in)
     locs_y = Float64.(locs_y_in)
 
-    # Scale to unit square
-    min_x, max_x = extrema(locs_x)
-    min_y, max_y = extrema(locs_y)
-    function scaler(z, a, b)
-        if (a - b) == 0.0
-            return 0.5
-        else
-            return 2.0 * ((z - a) / (b - a)) - 1.0
-        end
-    end
-    map!(z -> scaler(z, min_x, max_x), locs_x, locs_x)
-    map!(z -> scaler(z, min_y, max_y), locs_y, locs_y)
-
     # Determine sizes
-    #NODESIZE    = 0.25/sqrt(N)
-    #LINEWIDTH   = 3.0/sqrt(N)
-
     max_nodesize = NODESIZE / maximum(nodesize)
     nodesize *= max_nodesize
     max_edgelinewidth = EDGELINEWIDTH / maximum(edgelinewidth)
@@ -156,77 +152,143 @@ function gplot(g::AbstractGraph{T},
         nodestrokelw *= max_nodestrokelw
     end
 
+    # Scale to unit square
+    min_x, max_x = extrema(locs_x)
+    min_y, max_y = extrema(locs_y)
+    function scaler(z, a, b)
+        if (a - b) == 0.0
+            return 0.5
+        else
+            return ((z - a) / (b - a))
+        end
+    end
+    map!(z -> scaler(z, min_x, max_x), locs_x, locs_x)
+    map!(z -> scaler(z, min_y, max_y), locs_y, locs_y)
+
+    min_x = 0.1 -minimum([x - getcycle(nodesize, i) for (i, x) in enumerate(locs_x)])
+    max_x = maximum([x + getcycle(nodesize, i) for (i, x) in enumerate(locs_x)]) - 0.9
+    min_y = 0.1 -minimum([y - getcycle(nodesize, i) for (i, y) in enumerate(locs_y)])
+    max_y = maximum([y + getcycle(nodesize, i) for (i, y) in enumerate(locs_y)]) - 0.9
+
+    function rescaler(z, a, b)
+        if (a - b) == 0.0
+            return 0.5
+        else
+            r = maximum([a, b])
+            return ((z + r*(1+2r)) / (1+2r))
+        end
+    end
+    map!(z -> rescaler(z, min_x, max_x), locs_x, locs_x)
+    map!(z -> rescaler(z, min_y, max_y), locs_y, locs_y)
+
     # Create nodes
-    nodecircle = fill(0.4Compose.w, length(locs_x))
-    if isa(nodesize, Real)
-        	    for i = 1:length(locs_x)
-            	    	nodecircle[i] *= nodesize
-        	    end
-    	else
-        		for i = 1:length(locs_x)
-            	    	nodecircle[i] *= nodesize[i]
-        	    end
-    	end
-    nodes = circle(locs_x, locs_y, nodecircle)
+    rs = [getcycle(nodesize, i) for i in 1:length(N)]
+    nodes = circle(locs_x, locs_y, rs)
 
     # Create node labels if provided
-    texts = nothing
-    if nodelabel != nothing
+    texts = []
+    if !isnothing(nodelabel)
         text_locs_x = deepcopy(locs_x)
         text_locs_y = deepcopy(locs_y)
-        texts = text(text_locs_x .+ nodesize .* (nodelabeldist * cos(nodelabelangleoffset)),
-                     text_locs_y .- nodesize .* (nodelabeldist * sin(nodelabelangleoffset)),
-                     map(string, nodelabel), [hcenter], [vcenter])
+        texts = [
+            (
+                context(),
+                text(
+                    text_locs_x[i] + getcycle(nodesize, i) * (nodelabeldist * cos(nodelabelangleoffset)),
+                    text_locs_y[i] - getcycle(nodesize, i) * (nodelabeldist * sin(nodelabelangleoffset)),
+                    string(getcycle(nodelabel,i)), 
+                    hcenter, 
+                    vcenter
+                ),
+                fill(getcycle(nodelabelc,i)),
+                stroke(getcycle(nodelabelc, i)),
+                fontsize(getcycle(nodelabelsize, i)),
+            )
+            for i in 1:length(nodelabel)
+        ]
     end
+
     # Create edge labels if provided
-    edgetexts = nothing
+    edgetexts = []
     if !isempty(edgelabel)
         edge_locs_x = zeros(R, NE)
         edge_locs_y = zeros(R, NE)
-        for (e_idx, e) in enumerate(edges(g))
+        iter = isnothing(nodemapping) ? edges(g) : [e for e in edges(g) if ((src(e) in nodemapping) && (dst(e) in nodemapping))]
+        for (e_idx, e) in enumerate(iter)
             i = src(e)
             j = dst(e)
+            if !isnothing(nodemapping)
+                i = findfirst(nodemapping .== i)
+                j = findfirst(nodemapping .== j)
+            end
             mid_x = (locs_x[i]+locs_x[j]) / 2.0
             mid_y = (locs_y[i]+locs_y[j]) / 2.0
             edge_locs_x[e_idx] = (is_directed(g) ? (mid_x+locs_x[j]) / 2.0 : mid_x) + edgelabeldistx * NODESIZE
             edge_locs_y[e_idx] = (is_directed(g) ? (mid_y+locs_y[j]) / 2.0 : mid_y) + edgelabeldisty * NODESIZE
 
         end
-        edgetexts = text(edge_locs_x, edge_locs_y, map(string, edgelabel), [hcenter], [vcenter])
+        edgetexts = [
+            (
+                context(),
+                text(
+                    edge_locs_x[i], 
+                    edge_locs_y[i], 
+                    string(getcycle(edgelabel, i)), 
+                    hcenter, 
+                    vcenter
+                ),
+                fill(getcycle(edgelabelc, i)),
+                stroke(nothing),
+                fontsize(getcycle(edgelabelsize, i)),
+            )
+            for i in 1:length(edgelabel)
+        ]
     end
 
     # Create lines and arrow heads
-    lines, arrows = nothing, nothing
+    lines, arrows, stubs = nothing, nothing, nothing
     if linetype == "curve"
         if arrowlengthfrac > 0.0
-            curves_cord, arrows_cord = graphcurve(g, locs_x, locs_y, nodesize, arrowlengthfrac, arrowangleoffset, outangle)
+            curves_cord, arrows_cord = graphcurve(g, locs_x, locs_y, nodemapping, nodesize, arrowlengthfrac, arrowangleoffset, outangle)
             lines = curve(curves_cord[:,1], curves_cord[:,2], curves_cord[:,3], curves_cord[:,4])
             arrows = line(arrows_cord)
         else
-            curves_cord = graphcurve(g, locs_x, locs_y, nodesize, outangle)
+            curves_cord = graphcurve(g, locs_x, locs_y, nodemapping, nodesize, outangle)
             lines = curve(curves_cord[:,1], curves_cord[:,2], curves_cord[:,3], curves_cord[:,4])
         end
     else
         if arrowlengthfrac > 0.0
-            lines_cord, arrows_cord = graphline(g, locs_x, locs_y, nodesize, arrowlengthfrac, arrowangleoffset)
+            lines_cord, arrows_cord = graphline(g, locs_x, locs_y, nodemapping, nodesize, arrowlengthfrac, arrowangleoffset)
             lines = line(lines_cord)
             arrows = line(arrows_cord)
         else
-            lines_cord = graphline(g, locs_x, locs_y, nodesize)
+            lines_cord = graphline(g, locs_x, locs_y, nodemapping, nodesize)
+            if !isnothing(nodemapping)
+                stub_cord = stubline(g, locs_x, locs_y, nodemapping, nodesize)
+                stubs = line(stub_cord)
+            end
             lines = line(lines_cord)
         end
     end
 
-    compose(context(units=UnitBox(-1.2, -1.2, +2.4, +2.4)),
-            compose(context(), texts, fill(nodelabelc), stroke(nothing), fontsize(nodelabelsize)),
-            compose(context(), nodes, fill(nodefillc), stroke(nodestrokec), linewidth(nodestrokelw)),
-            compose(context(), edgetexts, fill(edgelabelc), stroke(nothing), fontsize(edgelabelsize)),
-            compose(context(), arrows, stroke(edgestrokec), linewidth(edgelinewidth)),
-            compose(context(), lines, stroke(edgestrokec), fill(nothing), linewidth(edgelinewidth)))
+    compose(
+            context(),
+            texts...,
+            (context(), nodes, fill(nodefillc), stroke(nodestrokec), linewidth(nodestrokelw)),
+            edgetexts...,
+            (context(), arrows, stroke(edgestrokec), linewidth(edgelinewidth)),
+            (context(), lines, stroke(edgestrokec), fill(nothing), linewidth(edgelinewidth)),
+            (context(), stubs, stroke(edgestrokec), fill(nothing), linewidth(edgelinewidth))
+    )
 end
 
 function gplot(g; layout::Function=spring_layout, keyargs...)
-    gplot(g, layout(g)...; keyargs...)
+    locs_x, locs_y = layout(g)
+    if haskey(keyargs, :nodemapping)
+        locs_x = [locs_x[i] for i in keyargs[:nodemapping]]
+        locs_y = [locs_y[i] for i in keyargs[:nodemapping]]
+    end
+    gplot(g, locs_x, locs_y; keyargs...)
 end
 
 # take from [Gadfly.jl](https://github.com/dcjones/Gadfly.jl)
